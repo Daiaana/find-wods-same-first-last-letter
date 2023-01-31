321fa597eb315927a2a676015424b9a09c8fd33a
daiaa <daiaa@LAPTOP-1QIJO96U>
2023-01-31 20:20:05 +0300

    Добавьте файлы проекта.

---
+++ number 5/number 5.cpp
@@ -0,0 +1,26 @@
+﻿// дана строка с пробелами, найти кол-во слов, которые начинаются и заканчиваются одной и той же буквой
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string str, word;
+	int s = 0;
+	string::size_type k = 0, pos = 0;
+	cout << "etner string" << endl;
+	getline(cin, str);
+	str = str + ' ';
+	k = str.find(" ", pos);
+	while (k != string::npos) {
+		word = str.substr(pos, k - pos);
+		if (word[0] == word[word.length() - 1])
+			s++;
+		pos = k + 1;
+		k = str.find(" ", pos);
+	}
+	cout << "k = " << s << endl;
+	return 0;
+}
+
